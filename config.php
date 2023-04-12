4124c641ce33378784d8c1197e3497a5f2c2a822
Juan <im.jan.caser@gmail.com>
2023-04-12 08:45:11 +0800

    Initial commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('APP_ID', '6ogpInMa8RfA7cx8XRTa6zfazooqI4o7');
+define('APP_SECRET', 'b790e865e0149bc7654fb5ac9d7d1a5f706e49acd9594c70d82cb036e210e23b');
+define('SHORT_CODE', '21663042');
